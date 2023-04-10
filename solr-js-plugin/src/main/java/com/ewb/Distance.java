d62d1e1b6ffba5aa281d3adc25c4e164eb1cf8bb
Lorena Calvo Bartolomé <100330717@alumnos.uc3m.es>
2023-04-10 16:01:42 +0200

    Updated Distance.

--- solr-js-plugin/src/main/java/com/ewb/Distance.java
+++ solr-js-plugin/src/main/java/com/ewb/Distance.java
@@ -2,36 +2,31 @@ package com.ewb;
 
 public class Distance {
 
-    Distance() {}
-
+    Distance() {
+    }
 
     /**
      * Gets the Kullback Leibler divergence.
+     * 
      * @param p P vector.
      * @param q Q vector.
      * @return The Kullback Leibler divergence between u and v.
      */
     public double KullbackLeiblerDivergence(double[] p, double[] q) {
 
-        boolean intersection = false;
         double k = 0;
 
         for (int i = 0; i < p.length; i++) {
             if (p[i] != 0 && q[i] != 0) {
-                intersection = true;
                 k += p[i] * Math.log(p[i] / q[i]);
             }
         }
-
-        // if (intersection)
-        //     return k;
-        // else
-        //     return Double.POSITIVE_INFINITY;
         return k;
     }
 
     /**
      * Gets the Jensen Shannon divergence.
+     * 
      * @param p U vector.
      * @param q V vector.
      * @return The Jensen Shannon divergence between u and v.
@@ -39,7 +34,8 @@ public class Distance {
     public double JensenShannonDivergence(double[] p, double[] q) {
 
         if (p.length != q.length) {
-            throw new IllegalArgumentException(String.format("Arrays have different length: p[%d], q[%d]", p.length, q.length));
+            throw new IllegalArgumentException(
+                    String.format("Arrays have different length: p[%d], q[%d]", p.length, q.length));
         }
 
         double[] m = new double[p.length];
@@ -50,5 +46,4 @@ public class Distance {
         return (KullbackLeiblerDivergence(p, m) + KullbackLeiblerDivergence(q, m)) / 2;
     }
 
-    
 }
